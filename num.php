dba85622f871349f1bb6ea166dccdad9ff064131
Maheknaaz <147641261+Maheknaaz@users.noreply.github.com>
2025-04-22 23:36:19 +0530

    Add files via upload

---
+++ num.php
@@ -0,0 +1,16 @@
+<?php
+// Ceiling value
+$number1 = 443.3432;
+echo "Ceiling of $number1 is: " . ceil($number1) . "<br>";
+
+// Floor value
+$number2 = 7543.829;
+echo "Floor of $number2 is: " . floor($number2) . "<br>";
+
+// Random number between 500 to 5000
+$random = rand(500, 5000);
+echo "Random number between 500 and 5000 is: $random . <br>";
+
+//Round value
+echo "round of $number1 is: " . round($number1) . "<br>";
+?>
